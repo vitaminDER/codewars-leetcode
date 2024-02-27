3ef2c6d6abfc542b6efc365bf30ea1c4ed3e8141
Efim Goncharov <efim.goncharov@bk.ru>
2024-02-28 03:10:46 +0500

    Task Kata8 Done

--- src/Kata.java
+++ src/Kata.java
@@ -10,11 +10,8 @@ public class Kata {
     }
 
     public static int makeNegative(final int x) {
-        if (x > 0)
-         return -x;
-        if(x==0)
-            return 0;
 
-        return x;
+
+        return (x > 0) ? -x : x;
     }
 }
