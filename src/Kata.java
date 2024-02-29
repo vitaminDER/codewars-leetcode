bfeaf40b2f1e7ef3973772692ad94ba879bcfa73
Efim Goncharov <efim.goncharov@bk.ru>
2024-02-29 20:50:21 +0500

    Convert boolean values to strings 'Yes' or 'No'. Kata8

--- src/Kata.java
+++ src/Kata.java
@@ -1,18 +1,13 @@
 public class Kata {
 
-    public static void main(String[] args) {
 
-        System.out.println(makeNegative(5));
-        System.out.println(makeNegative(0));
-        System.out.println(makeNegative(-5));
+    public static String boolToWord(boolean b)
+    {
 
-
+        return (!b) ? "No" : "Yes";
     }
-
-    public static int makeNegative(final int x) {
-
-
-        return (x > 0) ? -x : x;
-
+    public static void main(String[] args) {
+        System.out.println(boolToWord(true));
+        System.out.println(boolToWord(false));
     }
 }
