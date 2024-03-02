180bdaa8a6474125c6d07b4c7affc7ee3338f0bb
Efim Goncharov <efim.goncharov@bk.ru>
2024-03-02 22:54:27 +0500

    Convert a Number to a String! Kata 8

--- src/OppositeNumber.java
+++ src/OppositeNumber.java
@@ -2,7 +2,8 @@ public class OppositeNumber {
 
     public static int opposite(int number)
     {
-        return  (number > 0 || number < 0 ) ? -number : number;
+        return  -number;
     }
+
 
 }
