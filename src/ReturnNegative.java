f490c0a84851d1dd6315ebb3dc07114c3f52ee64
Efim Goncharov <efim.goncharov@bk.ru>
2024-03-02 22:55:36 +0500

    Return Negative Kata 8

--- src/ReturnNegative.java
+++ src/ReturnNegative.java
@@ -1,13 +1,10 @@
 public class ReturnNegative {
 
-    public static int makeNegative(final int x)
+    public static String numberToString(int num)
     {
-        return (x>0) ? -x : x;
-    }
+        return Integer.toString(num);
 
-    public static void main(String[] args) {
-        System.out.println(makeNegative(5));
-        System.out.println(makeNegative(-5));
+
     }
 
 }
