3a30ae0aa4c6852b08f934fcd1aae62404c08b84
Efim Goncharov <efim.goncharov@bk.ru>
2024-03-22 23:45:25 +0500

    Kata 8

---
+++ src/Kata_8/Solution.java
@@ -0,0 +1,11 @@
+package Kata_8;
+
+import javax.swing.*;
+//String repeat
+/*
+public class Solution {
+    public static String repeatStr(final int repeat, final String string) {
+        return string.repeat(repeat);
+    }
+
+}*/
