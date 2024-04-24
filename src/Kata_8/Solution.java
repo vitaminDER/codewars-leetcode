5a799f6a7c10d7035ee437dc4dde12ab750490e0
Efim Goncharov <efim.goncharov@bk.ru>
2024-04-25 02:42:15 +0500

    8 kata

--- src/Kata_8/Solution.java
+++ src/Kata_8/Solution.java
@@ -58,7 +58,14 @@ public class BasicOperations {
     }
 */
 
-
+//RETURNING STRINGS
+/*
+class Kata8 {
+    public static String greet(String name) {
+        return "Hello, " + name + " how are you doing today?";
+    }
+}
+*/
 
 
 
