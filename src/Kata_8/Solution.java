f3c0a8da73fc7283458915ef7e07f6c1bda7e4e2
Efim Goncharov <efim.goncharov@bk.ru>
2024-04-24 15:04:49 +0500

    8 kata

--- src/Kata_8/Solution.java
+++ src/Kata_8/Solution.java
@@ -35,6 +35,34 @@ public class SmallestIntegerFinder {
     }
 }*/
 
+//Basic Mathematical Operations
+/*
+public class BasicOperations {
+    public static Integer basicMath(String op, int v1, int v2) {
+
+        if(op == "+") {
+            return v1 + v2;
+        }
+        if (op == "*") {
+            return v1 * v2;
+        }
+        if (op == "-") {
+            return v1 - v2;
+        }
+        if (op == "/") {
+            return v1 / v2;
+        }
+        return 0;
+    }
+
+    }
+*/
+
+
+
+
+
+
 
 
 
