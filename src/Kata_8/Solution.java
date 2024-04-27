e46b296369c275643ca93bd3f9999233772c71e0
Efim Goncharov <efim.goncharov@bk.ru>
2024-04-28 03:18:15 +0500

    8 kata

--- src/Kata_8/Solution.java
+++ src/Kata_8/Solution.java
@@ -80,6 +80,28 @@ public class Banjo {
 }
 */
 
+//CALCULATE BMI
+/*
+public class Calculate {
+    public static String bmi(double weight, double height) {
+        double bmi = (weight / Math.pow(height, 2));
+        if (bmi <= 18.5) {
+            return "Underweight";
+        }
+        if (bmi <= 25.0) {
+            return "Normal";
+        }
+        if (bmi <= 30.0) {
+            return "Overweight";
+        }
+        if (bmi > 30) {
+            return "Obese";
+        }
+        return null;
+    }
+}
+*/
+
 
 
 
