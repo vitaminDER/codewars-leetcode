2dd8591b467abfcd712127334a207b3ca9964531
Efim Goncharov <efim.goncharov@bk.ru>
2024-05-02 02:18:57 +0500

    8 kata

--- src/Kata_8/Solution.java
+++ src/Kata_8/Solution.java
@@ -126,6 +126,21 @@ public class Kata {
 }
 */
 
+//Grasshopper - Summation
+/*
+public class GrassHopper {
+
+    public static int summation(int n) {
+        int sum = 0;
+        for(int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+}
+*/
+
+
 
 
 
