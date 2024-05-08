f2c88c2e50a8a49d1053f521f5a1cdd6ddea0f57
Efim Goncharov <efim.goncharov@bk.ru>
2024-05-08 21:10:18 +0500

    7 kata

---
+++ src/Kata_7/Kata7.java
@@ -0,0 +1,20 @@
+package Kata_7;
+
+//Vowel Count
+/*
+public class Vowels {
+
+    public static int getCount(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+
+            String strr = str.charAt(i) + "";
+
+            if (strr.equals("a") || strr.equals("e") || strr.equals( "i") || strr.equals( "o") || strr.equals( "u")) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+*/
