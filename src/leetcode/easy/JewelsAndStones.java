f0b4915aad91b44e5e70bcc7f73c6ae2fc5de41d
Efim Goncharov <efim.goncharov@bk.ru>
2025-02-05 20:49:31 +0500

    leetcode

---
+++ src/leetcode/easy/JewelsAndStones.java
@@ -0,0 +1,21 @@
+package leetcode.easy;
+
+/*
+* Jewels and Stones
+* */
+public class JewelsAndStones {
+    public int numJewelsInStones(String jewels, String stones) {
+
+        int count = 0;
+
+        for (int i = 0; i < jewels.length(); i++) {
+
+            for (int j = 0; j < stones.length(); j++) {
+
+                if (jewels.charAt(i) == stones.charAt(j))
+                    count++;
+            }
+        }
+        return count;
+    }
+}
