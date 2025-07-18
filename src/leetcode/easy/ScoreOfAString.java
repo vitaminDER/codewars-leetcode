2371a4cd7d978222e633b41c5a91c41d7b13ed5d
Efim Goncharov <efim.goncharov@bk.ru>
2025-07-18 21:00:10 +0500

    leetcode

---
+++ src/leetcode/easy/ScoreOfAString.java
@@ -0,0 +1,13 @@
+package leetcode.easy;
+/*
+* Score of a String
+* */
+public class ScoreOfAString {
+    public int scoreOfString(String s) {
+        int result = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+            result += Math.abs(s.charAt(i) - s.charAt(i + 1));
+        }
+        return result;
+    }
+}
